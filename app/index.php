4ec90d69af126bf9b8cb08fbb2112174756a5311
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-04 14:47:25 -0400

    Cleanup
    - Removed variables and chained all to single var

--- app/index.php
+++ app/index.php
@@ -9,20 +9,18 @@ function load_json(string $path)
 
 $products = collect(load_json('data/products.json')['products']);
 
-$coversAndWallets = $products->filter(function ($product) {
-  return collect(['Slim Cover', 'MagSafe Wallet'])->contains($product['product_type']);
-});
-
-$variants = $coversAndWallets->map(function ($product) {
-  return $product['variants'];
-})->flatten(1);
-
-
-$prices = $variants->map(function ($variant) {
-  return $variant['price'];
-});
-
-$totalCost = $prices->sum();
+$totalCost = $products
+  ->filter(function ($product) {
+    return collect(['Slim Cover', 'MagSafe Wallet'])->contains($product['product_type']);
+  })
+  ->map(function ($product) {
+    return $product['variants'];
+  })
+  ->flatten(1)
+  ->map(function ($variant) {
+    return $variant['price'];
+  })
+  ->sum();
 
 dd($totalCost);
 // 462.0
