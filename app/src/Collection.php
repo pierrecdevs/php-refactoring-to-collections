a3ae1c99be3b9399c142f3ff2c77d1e4e54d5512
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-04 14:11:15 -0400

    Added more basic functions
    - Added basic functions to Collections
    - This is in no way to replace the amazing Illuminate/Support
    collections

--- app/src/Collection.php
+++ app/src/Collection.php
@@ -2,15 +2,65 @@
 
 namespace Pierrecdevs\App;
 
+use ArrayAccess;
+use ArrayIterator;
 use Closure;
+use Countable;
+use IteratorAggregate;
+use Traversable;
 
-class Collection
+class Collection implements ArrayAccess, Countable, IteratorAggregate
 {
-  protected $items;
+  protected $items = [];
 
-  public function __construct($items)
+  public function __construct($items = [])
   {
     $this->items = $items;
+  }
+
+  // #region IteratorAggregate
+  public function getIterator(): Traversable
+  {
+    return new ArrayIterator($this->items);
+  }
+  // #endregion
+
+  // #region Countable
+  public function count(): int
+  {
+    return count($this->items);
+  }
+  // #endregion
+
+  // #region ArrayAccess
+  public function offsetExists(mixed $offset): bool
+  {
+    return array_key_exists($offset, $this->items);
+  }
+
+  public function offsetGet(mixed $offset): mixed
+  {
+    return $this->items[$offset];
+  }
+
+  public function offsetSet(mixed $offset, mixed $value): void
+  {
+    if ($offset == null) {
+      $this->items[] = $value;
+    } else {
+      $this->items[$offset] = $value;
+    }
+  }
+
+  public function offsetUnset(mixed $offset): void
+  {
+    unset($this->items[$offset]);
+  }
+  // #endregion
+
+  public function map(Closure $callback)
+  {
+    return new static(array_map($callback, $this->items));
   }
 
   public function filter(Closure $callback)
@@ -18,7 +68,7 @@ class Collection
     return new static(array_filter($this->items, $callback));
   }
 
-  public static function make($items)
+  public static function make(mixed $items = [])
   {
     return new static($items);
   }
@@ -28,8 +78,13 @@ class Collection
     return $this->items;
   }
 
-  public function contains($needle)
+  public function contains(mixed $needle): bool
   {
     return in_array($needle, $this->items);
+  }
+
+  public function reduce(Closure $callback, mixed $initial = null): mixed
+  {
+    return new static(array_reduce($this->items, $callback, $initial));
   }
 }
