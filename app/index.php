f7a061e3c60be907884bba9a524cd867ee4f82bf
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-04 14:42:23 -0400

    Using Symfony Collections
    - Now using Symfony Collections
    - Updated composer.json and lock

--- app/index.php
+++ app/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use Pierrecdevs\App\Collection;
-
 require_once 'vendor/autoload.php';
 
 function load_json(string $path)
@@ -9,25 +7,20 @@ function load_json(string $path)
   return json_decode(file_get_contents(__DIR__ . '/' . $path), true);
 }
 
-$products = Collection::make(load_json('data/products.json')['products']);
+$products = collect(load_json('data/products.json')['products']);
 
 $coversAndWallets = $products->filter(function ($product) {
-  return Collection::make(['Slim Cover', 'MagSafe Wallet'])->contains($product['product_type']);
+  return collect(['Slim Cover', 'MagSafe Wallet'])->contains($product['product_type']);
 });
 
 $variants = $coversAndWallets->map(function ($product) {
   return $product['variants'];
+})->flatten(1);
+
+
+$prices = $variants->map(function ($variant) {
+  return $variant['price'];
 });
-
-dd($variants);
-
-$prices = Collection::make([]);
-
-foreach ($coversAndWallets->toArray() as $product) {
-  foreach ($product['variants'] as $variant) {
-    $prices[] = $variant['price'];
-  }
-}
 
 $totalCost = $prices->sum();
 
