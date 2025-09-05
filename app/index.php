457df1b53a0d006d8a0a5f1fbcaabea0f494b899
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-05 07:58:20 -0400

    Refactored to using collections.
    - Dumping strings to arrays.
    - Later summing them up.

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+function binaryToDecimal($binary)
+{
+  return collect(str_split($binary))
+    ->reverse()
+    ->values()
+    ->map(function ($column, $exponent) {
+      return $column * (2 ** $exponent);
+    });
+}
+
+var_dump(binaryToDecimal("100110101")->all());
