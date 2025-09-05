8733089e5fae47b9f4c27960da73ccaa9628d91a
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-05 10:21:03 -0400

    Refactored to classes

--- app/index.php
+++ app/index.php
@@ -4,30 +4,45 @@ use Illuminate\Support\Collection;
 
 require_once 'vendor/autoload.php';
 
+class GitHubScore
+{
+  private Collection $events;
+
+  private function __construct(Collection | array $events)
+  {
+    $this->events = collect($events);
+  }
+
+  public static function score(Collection $events)
+  {
+    return (new static($events))->scoreEvents();
+  }
+
+  private function scoreEvents()
+  {
+    return $this->events
+      ->pluck('type')
+      ->map(function ($eventType) {
+        return $this->lookupEventScore($eventType);
+      })
+      ->sum();
+  }
+
+  private function lookupEventScore($eventType)
+  {
+    return collect([
+      'PushEvent' => 5,
+      'CreateEvent' => 4,
+      'IssuesEvent' => 3,
+      'CommitCommentEvent' => 2,
+    ])->get($eventType, 1);
+  }
+}
+
 function load_json($path)
 {
   return json_decode(file_get_contents(__DIR__ . '/' . $path), true);
 }
 
-function githubScore(Collection $events)
-{
-  return $events
-    ->pluck('type')
-    ->map(function ($eventType) {
-      return lookupEventScore($eventType);
-    })
-    ->sum();
-}
-
-function lookupEventScore($eventType)
-{
-  return collect([
-    'PushEvent' => 5,
-    'CreateEvent' => 4,
-    'IssuesEvent' => 3,
-    'CommitCommentEvent' => 2,
-  ])->get($eventType, 1);
-}
-
-$events = load_json('data/events.json');
-dd(githubScore(collect($events)));
+$events = collect(load_json('data/events.json'));
+dd(GitHubScore::score($events));
