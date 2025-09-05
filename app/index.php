01274a3d49310d9a2365825ac2b91a861aa20f21
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2025-09-05 07:59:25 -0400

    Finally sum it up

--- app/index.php
+++ app/index.php
@@ -9,7 +9,7 @@ function binaryToDecimal($binary)
     ->values()
     ->map(function ($column, $exponent) {
       return $column * (2 ** $exponent);
-    });
+    })->sum();
 }
 
-var_dump(binaryToDecimal("100110101")->all());
+var_dump(binaryToDecimal("100110101"));
